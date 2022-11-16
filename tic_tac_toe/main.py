db7031036b952c44536daf86994583d095ce2244
michsko <michal.skocovsky@protonmail.com>
2022-11-16 11:57:30 +0100

    tic tac toe check score and winner

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -9,7 +9,8 @@ print(f"Your name is.... {player_2} .... and your symbol is ..... o ......")
 
 player_1_score = 0
 player_2_score = 0
-
+winner = False
+moves = 0
 score = f" the score of your play is player 1 {player_1_score} : player 2 {player_2_score}"
 
 
@@ -55,22 +56,40 @@ def print_ground():
     print(play_ground[21], play_ground[22], play_ground[23])
     print(play_ground[31], play_ground[32], play_ground[33])
 
+
+def check_winner():
+    """Define checking the winner"""
+    winning_combinations = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [13, 23, 33],
+                           [12, 22, 32], [11, 21, 31], [11, 22, 33], [12, 22, 31]]
+    global winner
+    global player_1_score
+    global player_2_score
+
+    if player_1_all_moves in winning_combinations:
+        player_1_score += 1
+        print(f"Player ")
+
+    elif player_2_all_moves in winning_combinations:
+        winner = True
+        player_2_score += 1
+    else:
+        pass
+
 """ Play """
 
-winner = False
-moves = 0
+
 print_ground()
 while not winner:
     if moves <= 9:
         player_1_move()
         moves += 1
-
         print_ground()
+        check_winner()
 
         player_2_move()
         moves += 1
-
         print_ground()
+        check_winner()
     else:
         break
 
