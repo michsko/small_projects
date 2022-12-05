cd6aa45d59e6050fbe7d70c6f212f895682ff970
michsko <michal.skocovsky@protonmail.com>
2022-12-05 16:44:29 +0100

    bio/index/program

--- portfolio_website/main.py
+++ portfolio_website/main.py
@@ -6,6 +6,22 @@ app = Flask("__name__")
 def index():
     return render_template("index.html")
 
+@app.route("/program")
+def program():
+    return render_template("program.html")
+
+
+@app.route("/repertoire")
+def repertoire():
+    return render_template("repertoire.html")
+
+@app.route("/bio")
+def bio():
+    return render_template("bio.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
